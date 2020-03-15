49675a7f5eaecaac2bad839806270524746bf749
Clayton Brown <45186932+Whit3XLightning@users.noreply.github.com>
2020-03-15 15:22:43 -0700

    General Update
    1. Updated server endpoint to use serverip:port/dynamic.json
    2. edited config to add refreshtime to adjust how often the bot updates the player count
    3. added new output logs

--- index.js
+++ index.js
@@ -11,42 +11,16 @@ const http = __importStar(require("request"));
 const discord = __importStar(require("discord.js"));
 const bot = new discord.Client;
 const config = {
-    "token": "", //The Bot Token
-    "guild": "", //The current discord guild
-    "admin": "", //A dev team / admin role that's allowed to force a update
+    "token": "", //The token of your discord bot.
+    "guild": "", //The guild (id) with those \/ channels.
+    "devteam": "", // A admin / dev role that is allowed to force an update of the player count with 5m=update.
+    "refreshtime": 30, // how often the player count is updated in seconds.
     "servers": [
         "",
-        //"SERVERNAME IP:PORT DISCORDCHANNELID"
+        //"name ip:port DiscordVoiceChannelID"
     ]
 };
-let updatePlayerInterval = setInterval(() => updatePlayers(), 30000);
-function updatePlayers() {
-    config.servers.forEach(server => {
-        var args = server.split(" ");
-        let guild = bot.guilds.get(config.guild);
-        if (guild) {
-            var channel = guild.channels.get(args[2]);
-        }
-        http.get(`http://${args[1]}/info.json`, { json: true }, (err, res, info) => {
-            if (err) {
-                if (err.code == "ECONNREFUSED" || err.code == "ETIMEDOUT") {
-                    if (channel) {
-                        channel.setName(`${args[0]}: Offline`);
-                        return;
-                    }
-                }
-            }
-            http.get(`http://${args[1]}/players.json`, { json: true }, (err, res, players) => {
-                if (err)
-                    throw err;
-                if (channel) {
-                    channel.setName(`${args[0]}: ${players.length}/${info.vars.sv_maxClients}`);
-                }
-            });
-        });
-    });
-}
-bot.login(config.token).catch(console.error);
+console.log(`Starting bot...\n Created by Whit3Xlightning (https://whitelightning.dev)`);
 bot.on("message", message => {
     if (message.content == "5m=update" && message.member.roles.has(config.devteam)) {
         if (message.deletable) {
@@ -56,3 +30,28 @@ bot.on("message", message => {
         message.reply("Forced a update of the player count(s).");
     }
 });
+bot.login(config.token);
+let updatePlayerInterval = setInterval(() => updatePlayers(), config.refreshtime * 1000);
+function updatePlayers() {
+    config.servers.forEach(server => {
+        var args = server.split(" ");
+        let guild = bot.guilds.get(config.guild);
+        if (guild) {
+            var channel = guild.channels.get(args[2]);
+        }
+        http.get(`http://${args[1]}/dynamic.json`, { json: true }, (err, res, data) => {
+            if (err) {
+                if (err.code == "ECONNREFUSED" || err.code == "ETIMEDOUT") {
+                    if (channel) {
+                        channel.setName(`${args[0]}: Offline`);
+                        return;
+                    }
+                }
+            }
+            if (channel) {
+                channel.setName(`${data.clients} / ${data.sv_maxclients}`);
+            }
+            console.log(`${new Date().toISOString()}: ${data.clients} / ${data.sv_maxclients}`);
+        });
+    });
+}
